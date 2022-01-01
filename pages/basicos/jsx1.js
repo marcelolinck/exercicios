1aba28d5eeeb7672b9625162e1fd0e2436aa0cd8
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-01-01 13:10:48 -0400

    Aula 13

---
+++ pages/basicos/jsx1.js
@@ -0,0 +1,8 @@
+export default function jsx1diferente(){
+//Neste caso estamos colocando html dentro de uma funcao no react
+    return (
+        <div>
+            <h1>JSX #1</h1>
+        </div>
+    )
+}
