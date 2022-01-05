ae23b01d38cbac3106847f820bc3854d805a1e63
marcelolinck <40663647+marcelolinck@users.noreply.github.com>
2022-01-04 23:21:37 -0400

    Aula 49 - Componente Baseado em Classe

---
+++ components/Contador.jsx
@@ -0,0 +1,50 @@
+import { Component } from "react";
+
+export default class Contador extends Component {
+    //O simbolo de ?? simboliza um valor default que pode ser passado, nesse caso é 0
+    state = {
+        numero: this.props.valorInicial ?? 0,
+        passo: this.props.passo,
+    }
+
+    inc = () => {
+        this.setState({
+            numero: this.state.numero + this.state.passo
+
+        })
+    }
+    dec = () => {
+        this.setState({
+            numero: this.state.numero - this.state.passo
+
+        })
+    }
+
+    alteraPasso = (ev) => {
+        this.setState(
+            { passo: +ev.target.value }
+        )}
+
+    renderForm() {
+        return (
+            <>
+                <h2>Via valor setado: {this.state.numero}</h2>
+                <input type="number" min={1} max={1000} value={this.state.passo} onChange={this.alteraPasso} />
+                <button onClick={this.dec}>-</button>
+                <button onClick={this.inc}>+</button>
+            </>
+
+        )
+    }
+// A funcao render() serve para chamar o componente quando é montado via classe
+    render() {
+        return (
+            <div>
+                <h1>Contador (usando Classe)</h1>
+                <h2>Via props de outro formulário: {this.props.valorInicial}</h2>
+               {/* Possibilidade de montar o formulario via function */}
+                {this.renderForm()}
+            </div>
+        )
+    }
+}
---
+++ pages/classe/contador.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import Contador from "../../components/Contador";
+
+export default class ContadorPage extends Component{
+
+    render(){
+        return (
+            <div>
+                <Contador valorInicial={1000} passo={37}/>
+            </div>
+        )
+    }
+
+}
